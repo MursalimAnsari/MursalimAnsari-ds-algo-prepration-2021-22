f23e3f7ce3f22ab175f482a41c9ae8ffcc04a210
MursalimAnsari <42885623+MursalimAnsari@users.noreply.github.com>
2021-08-16 01:39:36 +0530

    Contains Duplicate Easy Problem

---
+++ src/main/java/GoogleProblems/ContainsDuplicates.java
@@ -0,0 +1,111 @@
+package GoogleProblems;
+
+/*
+     Contains Duplicates=>  Given an integer array nums, return true if any value
+      appears at least twice in the array,  and return false if every element is distinct.
+
+  Example 1:
+
+     Input: nums = [1,2,3,1]
+     Output: true
+
+  Example 2:
+
+     Input: nums = [1,2,3,4]
+     Output: false
+
+
+   Example 3:
+
+     Input: nums = [1,1,1,3,3,4,3,2,4,2]
+     Output: true
+
+
+ */
+
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicates {
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,6,2,8,1,3};
+
+//        boolean duplicate =containsDuplicate(nums);
+//        System.out.println(duplicate);
+
+//        boolean duplicate = containsDuplicateUsingBruteForce(nums);
+//        System.out.println(duplicate);
+
+
+          boolean duplicate = containsDuplicateUsingSet(nums);
+          System.out.println(duplicate);
+
+    }
+
+
+    //  Time Complexity O(n^2)
+    //  Space Complexity O(1)
+    private static boolean containsDuplicateUsingBruteForce(int[] nums) {
+
+        int n= nums.length;
+        if(nums.length == 0) return false;
+
+        for(int i = 0; i<n;i++){
+            for (int j=i+1;j<n;j++){
+                if(nums[i]==nums[j])
+                    return true;
+            }
+
+        }
+
+        return  false;
+    }
+
+    // TIME COMPLEXITY O(nlogn)
+    // Space Complexity O(1)
+
+    private static boolean containsDuplicate(int[] nums) {
+
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        if(nums.length == 0) return false;
+
+        for(int i=1;i<n;i++){
+
+            if(nums[i-1]==nums[i]) return true;
+
+        }
+        return false;
+    }
+
+
+
+  // Time Complexity  O(n)
+  // Space Complexity O(n)
+
+   private static boolean containsDuplicateUsingSet(int[] nums) {
+
+        // 1 2 3 1
+
+
+
+        Set<Integer> set = new HashSet<>();
+
+
+        for(int i: nums){
+            if(set.contains(i)) {
+                return true;
+            }
+            else {
+                set.add(i);
+            }
+
+        }
+        return false;
+    }
+
+}
