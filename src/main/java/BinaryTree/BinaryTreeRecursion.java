630888e44e1b3ae1f97ec7631bf1913adbf52573
MursalimAnsari <42885623+MursalimAnsari@users.noreply.github.com>
2021-08-20 22:57:52 +0530

    Binary Tree Traversal Iterative and Recursive

--- src/main/java/BinaryTree/BinaryTreeRecursion.java
+++ src/main/java/BinaryTree/BinaryTreeRecursion.java
@@ -1,8 +1,36 @@
 package BinaryTree;
 
-public class CreateTree {
 
-    TreeNode root =null;
+/*
+  INPUT  : 1 2 4 8 11 45 23
+  OUTPUT :
+  INORDER TRAVERSAL   :  8 2 11 1 45 4 23
+  PREORDER TRAVERSAL  :  1 2 8 11 4 45 23
+  POSTORDER TRAVERSAL :  8 11 2 45 23 4 1
+ */
+
+
+import java.util.Stack;
+
+public class BinaryTreeRecursion {
+
+    TreeNode root;
+
+    public static void main(String[] args) {
+
+      BinaryTreeRecursion btr = new BinaryTreeRecursion();
+      btr.createTree();
+//         inOrderTraversal(btr.root);
+//         System.out.println(" ");
+//         preOrderTraversal(btr.root);
+//        System.out.println(" ");
+//        postOrderTraversal(btr.root);
+
+        System.out.println("Iterative InOrder :");
+        inorderIterative(btr.root);
+
+
+    }
 
 
     static class TreeNode{
@@ -14,13 +42,6 @@ public class CreateTree {
         public TreeNode(int data) {
             this.data = data;
         }
-    }
-
-    public static void main(String[] args) {
-
-
-
-
     }
 
  private void createTree(){
@@ -43,6 +64,65 @@ public class CreateTree {
 
    }
 
-   
+   // Recursive solution to tree traversal
+   // 1. Inorder tree traversal...
 
+   public static void inOrderTraversal(TreeNode root){
+
+        if (root==null) return ;
+
+        // left -root - right
+        inOrderTraversal(root.left);
+        System.out.print(root.data +" ");
+        inOrderTraversal(root.right);
+
+   }
+
+    public static void preOrderTraversal(TreeNode root){
+
+        if (root==null) return ;
+
+        // root -left- right
+        System.out.print(root.data +" ");
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+
+    }
+
+
+    public static void postOrderTraversal(TreeNode root){
+
+        if (root==null) return ;
+
+        // root -left- right
+
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        System.out.print(root.data +" ");
+    }
+
+
+    private static  void inorderIterative(TreeNode root){
+        Stack<TreeNode> s= new Stack<>();
+
+        if(root==null) return;
+
+        // assign root to a new node temp...
+        TreeNode temp = root;
+
+        while(!s.isEmpty() || temp!=null){
+
+            if(temp!=null){
+                s.push(temp);
+                temp = temp.left;
+            } else {
+                temp = s.pop();
+                System.out.print(temp.data +" ");
+                temp = temp.right;
+            }
+
+        }
+
+    }
 }
+
