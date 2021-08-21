027b57666b0f458c2088f053106408a1b403d0fc
MursalimAnsari <42885623+MursalimAnsari@users.noreply.github.com>
2021-08-21 23:15:47 +0530

    Leetcode levelOrder Solution

--- src/main/java/BinaryTree/BinaryTreeTraversal.java
+++ src/main/java/BinaryTree/BinaryTreeTraversal.java
@@ -214,6 +214,41 @@ public class BinaryTreeTraversal {
          }
      }
 
+     /*
+       leetcode solution
+     class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+         List<List<Integer>> res = new ArrayList<>();
+
+        if(root == null) return res;
+
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for(int i=0; i<size;i++){
+                TreeNode current = q.remove();
+                currentLevel.add(current.val);
+                 if (current.left != null) q.add(current.left);
+                if(current.right!=null) q.add(current.right);
+            }
+
+               res.add(currentLevel);
+        }
+
+        return res;
+    }
+}
+
+
+      */
+
 
 }
 
