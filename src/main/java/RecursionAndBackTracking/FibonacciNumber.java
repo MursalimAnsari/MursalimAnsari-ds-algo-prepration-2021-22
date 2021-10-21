f9f03cf64e63f2809102cd24a91c632fda37265e
MursalimAnsari <42885623+MursalimAnsari@users.noreply.github.com>
2021-10-21 09:16:29 +0530

    init

--- src/main/java/RecursionAndBackTracking/FibonacciNumber.java
+++ src/main/java/RecursionAndBackTracking/FibonacciNumber.java
@@ -4,10 +4,20 @@ public class FibonacciNumber {
 
     public static void main(String[] args) {
 
-        int n=7;
+        int n=90;
 
-        System.out.println(fib(n));
+       // System.out.println(fib(n));
 
+        System.out.println(fibonacci(n));
+
+    }
+
+    private static double fibonacci(int n) {
+
+       if(n==0) return 0;
+       if(n==1 || n==2) return 1;
+
+       return fibonacci(n-1) + fibonacci(n-2);
     }
 
     private static int fib(int n) {
@@ -19,6 +29,7 @@ public class FibonacciNumber {
          if(n<=2)return 1;
 
          return fib(n-2)+fib(n-1);
+
 
     }
 
