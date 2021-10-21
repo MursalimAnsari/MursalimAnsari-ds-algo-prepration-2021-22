978eb46b7316e66162c870c9792a5999aff62d36
MursalimAnsari <42885623+MursalimAnsari@users.noreply.github.com>
2021-10-21 09:26:00 +0530

    fibonacci series

--- src/main/java/RecursionAndBackTracking/FibonacciNumber.java
+++ src/main/java/RecursionAndBackTracking/FibonacciNumber.java
@@ -4,11 +4,13 @@ public class FibonacciNumber {
 
     public static void main(String[] args) {
 
-        int n=90;
+        int n=9;
 
-       // System.out.println(fib(n));
+       System.out.println(fib(n));
 
-        System.out.println(fibonacci(n));
+       // System.out.println(fibonacci(n));
+        System.out.println();
+        fibonacciSeries(n);
 
     }
 
@@ -17,7 +19,7 @@ public class FibonacciNumber {
        if(n==0) return 0;
        if(n==1 || n==2) return 1;
 
-       return fibonacci(n-1) + fibonacci(n-2);
+       return fibonacci(n-2) + fibonacci(n-1);
     }
 
     private static int fib(int n) {
@@ -30,7 +32,24 @@ public class FibonacciNumber {
 
          return fib(n-2)+fib(n-1);
 
+    }
+
+
+
+    private  static  void fibonacciSeries(int n){
+
+        int n1 =0;
+        int n2= 1;
+        System.out.print(n1+ " " +n2 );
+
+        for (int i=2; i<=n; i++){
+            int n3 = n1 + n2;
+            System.out.print(" " +n3);
+            n1= n2;
+            n2= n3;
+        }
 
     }
+
 
 }
