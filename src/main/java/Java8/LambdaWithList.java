ab1688d1447d6b6cf0541eb6ac0629b1b36b3d74
MursalimAnsari <42885623+MursalimAnsari@users.noreply.github.com>
2021-10-25 17:39:20 +0530

    lambda expression

--- src/main/java/Java8/LambdaWithList.java
+++ src/main/java/Java8/LambdaWithList.java
@@ -1,4 +1,53 @@
 package Java8;
 
-public class LamdaWithList {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+public class LambdaWithList {
+
+    public static void main(String[] args) {
+
+
+        List<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(0);
+        list.add(5);
+        list.add(20);
+        list.add(45);
+        list.add(50);
+
+
+        System.out.println("List Elements before sorting :" + list);
+
+
+        // after sorting
+        Collections.sort(list);
+        System.out.println("Sorting Elements in Natural Order :" + list);
+
+
+        // sorting in descending order
+       // Collections.sort(list, new MyComparator());
+
+        // use lambda expression to sort
+        Collections.sort(list, (I1, I2)->(I1>I2) ?-1:(I1<I2)?1:0);
+
+        System.out.println("Sorting Using Lambda :" + list);
+
+
+    }
+
 }
+
+//     class MyComparator implements Comparator<Integer> {
+//
+//        @Override
+//        public int compare(Integer I1, Integer I2) {
+//            return (I1 > I2) ? -1 : (I1 < I2) ? 1 : 0;
+//        }
+//
+//
+//    }
