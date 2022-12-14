35fd6549d2bd2592f02198601030fa1a3b792434
MursalimAnsari <42885623+MursalimAnsari@users.noreply.github.com>
2022-12-14 12:43:06 +0530

    left rotation of array by k positions

--- src/main/java/arrays/easy/RotateArrayByKPositions.java
+++ src/main/java/arrays/easy/RotateArrayByKPositions.java
@@ -4,22 +4,29 @@ public class RotateArrayByKPositions {
 
     public static void main(String[] args) {
 
-         int [] arr = {-1,-3,6,2,5,8};
+         //int [] arr = {9,5,4,2,1};
+         int [] arr = {1,2,3,4,5,6,7};
          int k=2;
 
         System.out.println("before rotating array: ");
           printArray(arr);
 
+//        System.out.println();
+//          rotate_Array_Right_Method_one(arr, k);
+//        System.out.println("after rotating array by method one: ");
+//        printArray(arr);
+
+
+
+//        System.out.println();
+//        rotate_Array_Right_Method_Two(arr, k);
+//        System.out.println("after right rotating array by method two: ");
+//        printArray(arr);
+
+
         System.out.println();
-          rotate_Array_Right_Method_one(arr, k);
-        System.out.println("after rotating array by method one: ");
-        printArray(arr);
-
-
-
-        System.out.println();
-        rotate_Array_Right_Method_Two(arr, k);
-        System.out.println("after rotating array by method two: ");
+        rotate_Array_Left_Method_Two(arr,k);
+        System.out.println("after left rotating array by method two: ");
         printArray(arr);
 
     }
@@ -31,13 +38,26 @@ public class RotateArrayByKPositions {
         int n= arr.length;
         k = k%n;
 
-        reversArray(arr,0,n-1);
-        reversArray(arr,0,k-1);
-        reversArray(arr,k,n-1);
+        reverseArray(arr,0,n-k-1);
+        reverseArray(arr,n-k,n-1);
+        reverseArray(arr,0,n-1);
 
     }
 
-    private static void reversArray(int[] arr, int start, int end) {
+    private static void rotate_Array_Left_Method_Two(int[] arr, int k) {
+
+        //for cycle greater than the size of array
+        int n= arr.length;
+        k = k%n;
+
+        reverseArray(arr,0,k-1);
+        reverseArray(arr,k,n-1);
+        reverseArray(arr,0,n-1);
+
+    }
+
+
+    private static void reverseArray(int[] arr, int start, int end) {
 
         while(start<end){
 
